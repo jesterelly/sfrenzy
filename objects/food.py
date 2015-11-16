d891a39c948d73831ef5b0f4b74f5a0852636a36
pqftgs <pqftgs@gmail.com>
2015-11-15 23:12:58 -0600

    Re-factor food spawning

--- objects/food.py
+++ objects/food.py
@@ -1,26 +1,63 @@
-from bge import logic
-from random import randint, shuffle
-from time import time
+import bge
+import random
+import time
+
+
+class Food:
+    def __init__(self, own):
+        self.owner = own
+        own.collisionCallbacks.append(self.collide)
+
+        for c in own.children:
+            if c.name.endswith('armature'):
+                c.playAction('FoodScale_Fast', 0, 5)
+
+    def collide(self, other):
+        if 'floor' in other:
+            self.owner.endObject()
+            bge.logic.foodspawner.food.remove(self)
+
+    def update(self):
+        own = self.owner
+        if own.worldPosition[2] < 0.0:
+            own.endObject()
+            bge.logic.foodspawner.food.remove(self)
+            return
+
+        own.applyMovement((0, 0, -0.01), False)
+        own.applyRotation((0, 0, 0.017), False)
+
+
+class FoodSpawner:
+    def __init__(self, own):
+        bge.logic.foodspawner = self
+        self.owner = own
+        self.items = ['banana', 'bread', 'lemon', 'orange']
+        self.food = []
+        self.next_spawn_time = time.monotonic()
+        self.spawn_delay = 0.5  # Seconds
+
+    def update(self):
+        now = time.monotonic()
+        if now > self.next_spawn_time:
+            self.next_spawn_time = now + self.spawn_delay
+
+            obj = random.choice(self.items)
+            spawn = random.choice(self.owner.children)
+
+            scene = bge.logic.getCurrentScene()
+            ob = scene.addObject(obj, spawn)
+            self.food.append(Food(ob))
+
+        for f in list(self.food):
+            f.update()
 
 
 def main(cont):
-    scene = logic.getCurrentScene()
-    spawner = cont.owner
-    spawners = [ob for ob in spawner.children]
-    shuffle(spawners)
+    own = cont.owner
+    fs = own.get('FoodSpawner', None)
+    if fs is None:
+        own['FoodSpawner'] = FoodSpawner(own)
+        return
 
-    if not hasattr(logic, 'zero_t'):
-        logic.zero_t = time()
-        logic.done = 0
-        logic.score = 0
-
-    t = time() - logic.zero_t
-
-    items = ['banana', 'bread', 'lemon', 'orange']
-    num = round(t / 100000) + 3
-
-    if round(t) != logic.done:
-        for e in range(num):
-            obj = items[randint(0,len(items)-1)]
-            scene.addObject(obj, spawners[e], 1500)
-        logic.done = round(t)
+    fs.update()
